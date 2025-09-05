79c71950d2af6c42c03b8cda32fe27796a82fbb2
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-05 19:40:42 +0900

    🎨 코드 포맷팅 (#1)

--- configs/settings.py
+++ configs/settings.py
@@ -27,8 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'corsheaders',
+    'rest_framework',
     'rest_framework_simplejwt',
 ]
 
