58d75024630a41669bdcc9838d4ca0d549a50389
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-09-05 22:49:38 +0900

    🔀 로그인 API 추가 (#8)
    * ♻️ User 모델을 직접 import하지 않고, get_user_model() 메서드 사용 (#7)
    
    * ♻️ UserSerializer id 필드 read_only 지정 (#7)
    
    * 🔨 LoginSerializer 추가 (#7)
    
    * 🔨 format_timestamp_iso (#7)
    
    * ✨ 로그인 API 추가 (#7)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from .models import User
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','email','password',)
+        read_only_fields = ('id',)
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data:dict):
@@ -13,3 +16,25 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            raise serializers.ValidationError(detail='이메일 또는 비밀번호가 일치하지 않아요.')
+        if not user.is_active:
+            raise serializers.ValidationError(detail='비활성화된 계정이에요.')
+
+        attrs['user'] = user
+        return attrs
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.settings import api_settings
+from utils.helpers import format_timestamp_iso
+from .serializers import LoginSerializer
 
-# Create your views here.
+class AccountsLoginRoot(APIView):
+    def post(self, request:HttpRequest, format=None):
+        serializer = LoginSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        refresh_token = RefreshToken.for_user(user)
+        access_token = refresh_token.access_token
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                'grant_type': api_settings.AUTH_HEADER_TYPES[0],
+                'access': {
+                    'token': str(access_token),
+                    'expire_at': format_timestamp_iso(access_token['exp']),
+                },
+                'refresh': {
+                    'token': str(refresh_token),
+                    'expire_at': format_timestamp_iso(refresh_token['exp']),
+                },
+            },
+        )
