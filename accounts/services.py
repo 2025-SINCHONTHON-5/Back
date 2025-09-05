e2d2acd0daca2013123616f584610dca29aa8a31
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-05 23:13:08 +0900

    🔨 UserService 추가 (#9)

--- accounts/services.py
+++ accounts/services.py
@@ -1,1 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpRequest
+from utils.helpers import get_instance_or_404
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+class UserService:
+    def __init__(self, request:HttpRequest, pk:int|None=None):
+        instance = get_instance_or_404(User, pk=pk, error_message='사용자를 찾을 수 없어요.') if pk else None
+        self.request = request
+        self.instance = instance
+        self.serialiser = UserSerializer(instance, data=request.data)
