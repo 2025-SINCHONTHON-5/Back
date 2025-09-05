c57ed5fbd1be9304020fdfb1037f8591888af568
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-05 22:40:36 +0900

    🔨 LoginSerializer 추가 (#7)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 User = get_user_model()
@@ -16,3 +16,25 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            raise serializers.ValidationError(detail='이메일 또는 비밀번호가 일치하지 않아요.')
+        if not user.is_active:
+            raise serializers.ValidationError(detail='비활성화된 계정이에요.')
+
+        attrs['user'] = user
+        return attrs
