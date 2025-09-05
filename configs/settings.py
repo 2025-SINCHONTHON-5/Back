e8790c4e8333de692fb668dc4fd139fedde218db
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-05 19:33:49 +0900

    🔧 media 설정 (#1)

--- configs/settings.py
+++ configs/settings.py
@@ -100,6 +100,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Media files
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
