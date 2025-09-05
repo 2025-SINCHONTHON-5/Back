24058f8bbc50940cc6f41b96f43ba9a4245299d1
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-09-06 00:00:27 +0900

    🔀 회원가입 API 추가 (#11)
    * 🎨 클래스 이름 변경 (#9)
    
    * 🔨 get_instance_or_404 추가 (#9)
    
    * 🔨 UserService 추가 (#9)
    
    * 🔨 validate_data, validate_unique 데코레이터 추가 (#9)
    
    * ✨ 회원가입 API 추가 (#9)
    
    * ♻️ 공통 로직을 JWTService로 분리 (#9)
    
    * 🐛 오타 수정 (#9)

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,4 +1,7 @@
 from functools import wraps
+from django.db import IntegrityError
+from rest_framework import status
+from rest_framework.response import Response
 
 def example(func):
     """
@@ -8,4 +11,36 @@ def example(func):
     def wrapper(self, *args, **kwargs):
         # 여기에 데코레이터 코드를 작성하세요.
         return func(self, *args, **kwargs)
+    return wrapper
+
+def validate_data(service_func):
+    """
+    클라이언트가 전송한 데이터가 유효한지 검증합니다.
+    Service에서 사용해 주세요.
+    """
+    @wraps(service_func)
+    def wrapper(self, *args, **kwargs):
+        if not self.serializer.is_valid():
+            return Response(
+                self.serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return service_func(self, *args, **kwargs)
+    return wrapper
+
+def validate_unique(service_func):
+    """
+    클라이언트의 요청이 UNIQUE 제약조건을 준수하는지 검증합니다.
+    Service에서 사용해 주세요.
+    """
+    @wraps(service_func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return service_func(self, *args, **kwargs)
+        except IntegrityError as error:
+            if "UNIQUE constraint failed" in str(error):
+                return Response(
+                    {"detail":"이미 존재하는 값이에요."},
+                    status=status.HTTP_409_CONFLICT,
+                )
     return wrapper
