2edcf8539114e36ee78a5b6d9a03bdb89331beef
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-05 23:12:30 +0900

    🔨 get_instance_or_404 추가 (#9)

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timezone
+from django.db.models import Model
+from rest_framework.exceptions import NotFound
 
 def format_timestamp_iso(timestamp):
     result_datetime = datetime.fromtimestamp(timestamp, tz=timezone.utc)
@@ -6,3 +8,9 @@ def format_timestamp_iso(timestamp):
     if "+00:00" in result_iso:
         return result_iso.replace("+00:00", "Z")
     return result_iso
+
+def get_instance_or_404(model:Model, pk:int, error_message:str):
+    try:
+        return model.objects.get(pk=pk)
+    except model.DoesNotExist:
+        raise NotFound(detail=error_message)
